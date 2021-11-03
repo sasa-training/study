682c972e6e8d193642ba718e72ddcbcd0323ed6d
sasa-training <ste523887@gmail.com>
2021-11-03 19:03:23 +0900

    fix-3rd

--- dist/js/main.js
+++ dist/js/main.js
@@ -8,7 +8,7 @@ $(function() {
     $('nav').removeClass('open_toggle');
   }
   });
-// リロード時
+  // リロード時
   // ウィンドウサイズ768px以下の場合、クラスを削除
   if (window.matchMedia( '(max-width: 700px)' ).matches) {
     } else {
@@ -18,39 +18,39 @@ $(function() {
       });
   };
   // リサイズ時
-$(window).resize(function(){
-  let x = $(window).width();
-  let y = 700;
-  if (x <= y) {
+  $(window).resize(function(){
+  let $windowWidth = $(window).width();
+  let $breakpoints = 700;
+  if ($windowWidth <= $breakpoints) {
   }
   else {
     $('nav').removeClass('open_toggle');
     $('button').removeClass('open_toggle');
   }
-});
-    // スライダー
-    let mySwiper = new Swiper ('.swiper', {
-        centeredSlides: true,
-          loop: true,
-          speed: 1000,
-          spaceBetween: 0,
-          autoplay: {
-              delay: 3000,
-          },
-        breakpoints:
-                {
-                    980: { 
-                        slidesPerView: 3,
-                    },
-                    768: {  
-                        slidesPerView: 2,
-                    },
-                    600: { 
-                        slidesPerView: 1.5,
-                    },
-                    0: { 
-                        slidesPerView: 1,
-                    },
-                }
-      });
+  });
+  // スライダー
+  let mySwiper = new Swiper ('.swiper', {
+    centeredSlides: true,
+      loop: true,
+      speed: 1000,
+      spaceBetween: 0,
+      autoplay: {
+          delay: 3000,
+      },
+    breakpoints:
+    {
+      980: { 
+        slidesPerView: 3,
+      },
+      768: {  
+        slidesPerView: 2,
+      },
+      600: { 
+        slidesPerView: 1.5,
+      },
+      0: { 
+        slidesPerView: 1,
+      },
+    }
+  });
 });
